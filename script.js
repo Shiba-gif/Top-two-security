61abf42144b312c35d8d0109bb1d4fa7c8039a0a
Shiba-gif <luuhshiba@gmail.com>
2025-08-07 01:38:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showQuote() {
+  alert("Thank you! Our team will contact you shortly for a quote.");
+}
+
+function sendForm(event) {
+  event.preventDefault();
+  alert("Thank you for contacting us! We'll reply to your message shortly.");
+}
